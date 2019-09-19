3a140ac97d31a18e628152d9ca474aa192f8406c
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-09-19 21:27:26 +0200

    WIP

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
-$name = 'Sergi';
+// ARRAYS/VECTORS
+
+$classe = [
+    'Sergi',
+    'Karim',
+    'Joan',
+    'Carlos',
+    'Genis',
+    'Ayram',
+    'Gerard',
+    'Dídac'
+];
+//require 'model.php';
 
 // include i require són però require peta si fitxer no existeix
+// VISTA
 require 'index.template.php';
 
