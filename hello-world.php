773c17731d585f6991259306f0cd78155d510e99
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-09-19 20:56:23 +0200

    WIP

--- hello-world.php
+++ hello-world.php
@@ -2,3 +2,15 @@
 $greeting = 'Hola';
 echo $greeting;
 
+$name = 'Sergi';
+//OCO javascript és + CONCATENAR
+echo 'Hola ' . $greeting;
+
+//OCO javascript és +
+echo 'Hola $greeting';
+echo "Hola $greeting";
+
+echo "Hola {$greeting}que tal";
+
+
+
