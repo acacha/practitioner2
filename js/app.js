9d8a51a832be959bdd1fcbc435616ec342c3bd46
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-09-20 16:23:59 +0200

    WIP

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// console.log('Hola mon')
+
+console.log(document)
+
+console.log(document.body)
+
+const name = 'Sergi'
+
+import greeting from './js/templates/greeting'
+
+console.log(greeting);
+
+
+// const template = `
+// {/* <h1>Hola ${name}!</h1> */}
+// `
+
+// document.body.innerHTML = "Hola " + name
+
+document.body.innerHTML = template
+
+// import i export
