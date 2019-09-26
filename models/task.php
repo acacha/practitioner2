81422f307476d6f0f7b66a7e09ecc35887e74326
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-09-26 18:57:03 +0200

    WIP

--- models/task.php
+++ models/task.php
@@ -1,12 +1,12 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=tasks','debian-sys-maint', 'RcGXsSqsnRy6nQn1');
+    $pdo = new PDO(
+        'mysql:host=127.0.0.1;dbname=tasks','debian-sys-maint', 'RcGXsSqsnRy6nQn1');
 } catch (PDOException $e) {
     die($e->getMessage());
 }
 
-// die(var_dump($pdo));
 
 $query = $pdo->prepare('SELECT * FROM tasks');
 
@@ -14,13 +14,4 @@ $query->execute();
 
 $results = $query->fetchAll();
 
-// die(var_dump($results[0]));
-// die(var_dump($results));
-
 $task = $results[0];
-
-// $task = [
-//     'name' => 'Comprar llet',
-//     'description' => 'Al lidl...',
-//     'completed' => false,
-// ];
