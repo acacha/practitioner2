e78cc8964b0047cb39e78c00b530e2a13a625f8b
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-09-26 18:37:56 +0200

    First example using Database PDO

--- models/task.php
+++ models/task.php
@@ -1,6 +1,26 @@
 <?php
-$task = [
-    'name' => 'Comprar llet',
-    'description' => 'Al lidl...',
-    'completed' => false,
-];
+
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=tasks','debian-sys-maint', 'RcGXsSqsnRy6nQn1');
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
+// die(var_dump($pdo));
+
+$query = $pdo->prepare('SELECT * FROM tasks');
+
+$query->execute();
+
+$results = $query->fetchAll();
+
+// die(var_dump($results[0]));
+// die(var_dump($results));
+
+$task = $results[0];
+
+// $task = [
+//     'name' => 'Comprar llet',
+//     'description' => 'Al lidl...',
+//     'completed' => false,
+// ];
